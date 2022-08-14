98c0668b53a9e27bbd5937eb8a361097c29900f7
BhushanBadhe <bhushanbadhe.2581@gmail.com>
2022-08-14 08:12:48 +0530

    Section 9 : Anagrams Completed

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,6 +8,48 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+//Solution no 1 
+function anagrams(stringA, stringB){
+
+    return cleanString(stringA) === cleanString(stringB);
+
+}
+
+function cleanString(str){
+    return str.replace(/[^\w]/g,'').toLowerCase().split('').sort().join('');
+}
+
 
 module.exports = anagrams;
+
+//Solution no 2
+// function anagrams(stringA, stringB) {
+//     const aCharMap = buildCharMap(stringA);
+//     const bCharmap = buildCharMap(stringB);
+
+
+//     if(Object.keys(aCharMap).length !== Object.keys(bCharmap).length){
+//         return false;
+//     }
+//     for(let char in aCharMap){
+
+//             if(aCharMap[char]!== bCharmap[char]){
+//                 return false;
+//             }
+//     }
+//     return true;
+
+// }
+
+
+// function buildCharMap(str){
+
+//     let charMap = {};
+
+//     for(let char of str.replace(/[^\w]/g)){
+
+//         charMap[char] = charMap[char] + 1 | 1;
+//     }
+
+//     return charMap;
+// }
